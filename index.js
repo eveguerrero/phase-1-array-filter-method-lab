a600e641ee576e84805de7854b9c76513779b759
Eveline Guerrero <evelineguerrero23@gmail.com>
2021-12-15 20:31:05 -0500

    commit tree c82985926266c4d95389c9ea6ab320e911cb6941

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(names, driver){
+    
+  return  names.filter(name => name.toLowerCase() === driver.toLowerCase() )
+     
+   }
+//returns all f=drivers whose name begins with the letters;
+function fuzzyMatch(drivers, letters){
+return drivers.filter(driver => driver.slice(0,2) === letters)
+}
+
+function matchName(driverArray, string){
+    return driverArray.filter(driver => driver.name === string);
+}
+
+
+
